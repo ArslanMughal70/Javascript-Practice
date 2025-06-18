25c4c7e0b0f178df822680c04c937aa7ea4950e7
Arslan Mughal <94012958+ArslanMughal70@users.noreply.github.com>
2025-06-18 05:26:26 +0500

    Starting basic funtioncality

---
+++ javascript.js
@@ -0,0 +1,81 @@
+// Operators
+
+// food= Number(prompt("Enter your Food Price"));
+// TipPercentage = Number(prompt("Tip Amount") / 100) ;
+
+// TipAmount = food * TipPercentage ;
+// console.log("Food Amount: " + food)
+// console.log("Tip Amount: " + TipAmount)
+
+// TotalAmount = food + TipAmount
+// console.log("Total Amount: "+ TotalAmount)
+
+
+// -----------------------------------------------------------
+
+/*  Data Types (String, Number)
+Number= 1,2,3,4,5,6,7,8
+String= "hello", "My Name Admin" 
+Arrays = []
+Objects = {}
+Boolean = True / False
+*/
+
+
+/*  Math Operation
+Multiply *
+Divide /
+Exponent **
+Modulo/Remainder  % 5%2 = 1
+Add +
+Subtract -
+*/
+
+
+/* Math Methods
+Floor - Rounds Down  110.25 = 110
+Ceil - Rounds Up   110.25 = 111
+Random - gives you number between 0 to 1
+*/
+
+// RandomNumber = Math.floor(Math.random() * 3);
+// console.log(RandomNumber)
+
+
+// Baby Weather App
+// if rain 'Grab your umbrella'
+// else 'Wear your sunglasses'
+
+
+// let Weather = prompt("How is the weather? rainy / sunny")
+// if (Weather == 'rainy'){
+//     console.log("Grab your umbrella")
+// }else{
+//     console.log('Wear your sunglasses')
+// }
+
+
+
+// Conditional Operators
+//  ==, ===, >, <, <=, >=, !=, !==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
